2e75aea4e475a8abb6102e1e28c3c938fd210e5b
chauchaunmt <65859033+chauchaunmt@users.noreply.github.com>
2020-11-08 21:04:54 +0700

    Hiểu được vai trò cũng như cách cấp phép quyền người sử dụng, cách sử dụng roles và permissions khi tạo enums

---
+++ src/main/java/com/chauchau/nmt/security/ApplicationUserRole.java
@@ -0,0 +1,18 @@
+package com.chauchau.nmt.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.chauchau.nmt.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+  STUDENT(Sets.newHashSet()),
+  ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+
+  private final Set<ApplicationUserPermission> permissions;
+
+  ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+    this.permissions = permissions;
+  }
+}
